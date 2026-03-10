be92153c0233ef6407d20df882b7ee63945483e9
곽수호 <shsm0722@naver.com>
2026-03-11 02:34:06 +0900

    하노이 타워

--- hanoiTower/hanoiTower.cpp
+++ hanoiTower/hanoiTower.cpp
@@ -8,13 +8,18 @@
 // Complete the function below
 int hanoi(int n, char from, char to, char aux) {
     // Base case: If only one disk, move it and return 1
-
+    if (n == 1) {return 1;}
     // Recursive case:
     // 1. Move (n-1) disks from 'from' to 'aux' using 'to' as auxiliary
     // 2. Move the nth disk from 'from' to 'to'
     // 3. Move (n-1) disks from 'aux' to 'to' using 'from' as auxiliary
-
+    int moves=0;
+    moves += hanoi(n-1, from, aux, to);//1번 동작. n-1개를 옮겨놓음
+    //n번째 원판이 시작점에서 옮길 수 있는 상태로 있음.
+    moves++; //n번째 원판을 시작점에서 종점으로 옮김. = 2번 동작
+    moves += hanoi(n-1,aux, to,from); //3번 동작. 임시기둥에 옮겨놓은 나머지 n-1개 원판들을 목적지로 옮김.
     // Return total number of moves
+    return moves;
 }
 
 int main() {
